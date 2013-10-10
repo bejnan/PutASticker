c9695463d9571494ec32b29f6361777fd9330d6c
Jakub Banaszewski <bananow@gmail.com>
2013-10-10 15:15:41 +0200

    New 2 activities

--- PutASticker/src/com/putasticker/StickActivity.java
+++ PutASticker/src/com/putasticker/StickActivity.java
@@ -1,8 +1,10 @@
 package com.putasticker;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class StickActivity extends Activity {
 
@@ -17,6 +19,18 @@ public class StickActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.stick, menu);
 		return true;
+	}
+	
+	public void saveSticker(View view)
+	{
+		Intent intent = new Intent(this,SavedStickActivity.class);
+		startActivity(intent);
+	}
+	
+	public void closeSticker(View view)
+	{
+		Intent intent = new Intent(this,StickerListActivity.class);
+		startActivity(intent);
 	}
 
 }
