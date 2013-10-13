0ceeaef82a5a470afb3364c55fe4b891aaddcd8e
Jakub Banaszewski <bananow@gmail.com>
2013-10-13 21:41:03 +0200

    Small changes in app (nothing new)

--- PutASticker/src/com/putasticker/SavedStickActivity.java
+++ PutASticker/src/com/putasticker/SavedStickActivity.java
@@ -2,14 +2,16 @@ package com.putasticker;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class SavedStickActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_save_stick);
+		setContentView(R.layout.activity_saved_stick);
 	}
 
 	@Override
@@ -17,6 +19,12 @@ public class SavedStickActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.save_stick, menu);
 		return true;
+	}
+	
+	public void closeSticker(View view)
+	{
+		Intent intent = new Intent(this,StickerListActivity.class);
+		startActivity(intent);
 	}
 
 }
